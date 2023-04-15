d87b7a8b9b88edca52c1b7553b9a9c5494ebd233
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-15 16:10:44 +0300

    Organize code for show modal and close modal (include comments)

--- js/script.js
+++ js/script.js
@@ -8,17 +8,19 @@ const modalWindow = select('.modal-window');
 const closeModalBtn = select('.close-modal');
 const overlay = select('.overlay');
 
+// Show modal
 const showModal = function () {
   modalWindow.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
 
+showBtns.forEach(btn => btn.addEventListener('click', showModal));
+
+// Close modal
 const closeModal = function () {
   modalWindow.classList.add('hidden');
   overlay.classList.add('hidden');
 };
-
-showBtns.forEach(btn => btn.addEventListener('click', showModal));
 
 closeModalBtn.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
