5367102e822ba773ae2ce66a6bd2689e8ad1c37a
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-15 13:23:12 +0300

    Show modal window

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
-const showBtns = document.querySelectorAll('.btn');
+const showBtns = document.querySelector('.btns');
 const modalWindow = document.querySelector('.modal-window');
 const closeModalBtn = document.querySelector('.close-modal');
+
+showBtns.addEventListener('click', function () {
+  modalWindow.classList.add('show');
+});
