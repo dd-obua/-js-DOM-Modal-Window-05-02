97e128c116868bb0e8410e9ff82f13510e5a2d75
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-15 14:13:27 +0300

    Active strict mode

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const showBtns = document.querySelectorAll('.btn');
 const modalWindow = document.querySelector('.modal-window');
 const closeModalBtn = document.querySelector('.close-modal');
