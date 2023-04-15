cfc438712dc76b9172e4e02cdf13f579f675f938
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-15 16:06:54 +0300

    Refactor show model by using a function

--- js/script.js
+++ js/script.js
@@ -8,17 +8,17 @@ const modalWindow = select('.modal-window');
 const closeModalBtn = select('.close-modal');
 const overlay = select('.overlay');
 
-showBtns.forEach(btn => {
-  btn.addEventListener('click', () => {
-    modalWindow.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
-});
+const openModal = function () {
+  modalWindow.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
 const closeModal = function () {
   modalWindow.classList.add('hidden');
   overlay.classList.add('hidden');
 };
+
+showBtns.forEach(btn => btn.addEventListener('click', openModal));
 
 closeModalBtn.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
