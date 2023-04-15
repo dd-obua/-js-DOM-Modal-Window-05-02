5a35b8c3c524463e74341885bbdf2966c1eaebde
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-15 14:55:30 +0300

    Remove hidden class from overlay on button click

--- js/script.js
+++ js/script.js
@@ -3,10 +3,12 @@
 const showBtns = document.querySelectorAll('.btn');
 const modalWindow = document.querySelector('.modal-window');
 const closeModalBtn = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
 
 showBtns.forEach(btn => {
   btn.addEventListener('click', () => {
     modalWindow.classList.remove('hidden');
+    overlay.classList.remove('hidden');
   });
 });
 
